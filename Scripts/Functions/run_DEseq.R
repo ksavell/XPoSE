07bb1042ca785c43ed410588f343343e84c3efd6
DJT333 <135276784+DJT333@users.noreply.github.com>
2023-07-11 12:11:51 -0400

    Update run_DEseq.R
    The wrong function was copied for this. 
    
    I added a new change to this that reduces the lines. Apparently as.formula exists and it's extremely useful for our purposes

--- Scripts/Functions/run_DEseq.R
+++ Scripts/Functions/run_DEseq.R
@@ -1,30 +1,68 @@
-#' Get Result Tibbles 2
-#' 
-#' This version combines prep_DEseq and get_result_tbl. Additionally, this has 
-#' default parameters to make your life easier (half the time). The meta_tbl 
-#' param can be left alone regardless of object unless you want to use different
-#' metadata table. 
+#' Run DEseq
 #'
-#' @param data_tab Aggregate table for the given object, returned by 'get_cnt_tbls'
-#' @param object SE
-#' @param dataname name of object, used a lot for once
-#' @param meta_tbl The table of metadata
+#' @param tbl_lst list of the psuedobulk counts
+#' @param meta_tbl table of metadata
+#' @param object Seurat object to perform analysis on
 #'
-#' @return a result table
+#' @return returns aggegate list of the result tables (they are tibbles)
 #' @export
 #'
 #' @examples
-#' #makes result table for a cluster in the glut object
-#' my_results <- run_DEseq()
-#' #makes result table for a cluster in the GABA object
-#' my_results <- run_DEseq(gaba_tabs, gaba, "Gaba")
-#' #result table for a cluster in glut but w/ a different metadata table
-#' my_results <- run_DEseq(meta_tbl = my_meta_tbl)
-run_DEseq <- function(data_tab = glut_tabs, object = glut, 
-                      dataname = "glut", meta_tbl = metaData){
-  #Gets count table
-  cnt_tbl <- prep_DEseq(data_tab, object, dataname)
-  
-  #gets and returns result table
-  return(get_result_tbl(meta_tbl, cnt_tbl, object))
+#' #Makes result tibbles for my_tbls list
+#' my_tibbs <- run_DEseq(my_tbls, meta_data, glut)
+run_DEseq <- function(tbl_lst, meta_tbl, object){
+    #our base list to hold tibbles, will be returned at function's end
+    agg_res <- list()
+    
+    #finds factor used in the list for user
+    for (name in colnames(object@meta.data)){
+        if (unique(meta_tbl[,1])[1] %in% unique(object@meta.data[name][, 1])){
+          factor <- name
+      }
+    }
+    
+    #Gets user's input for contrast
+    cat("Enter 2 comparisons, should be same as before")  #<- This may need to 
+                                                          #   be changed if more 
+                                                          #   wanted
+    contrast_vect <- c(factor, readline("Experimental Group: "), 
+                                readline("Control Group: "))
+    
+    
+    #Makes factor a formula for the design
+    form_fact <- paste("~", factor, sep = "")
+    form_fact <- as.formula(form_fact)
+    
+    #verifies user input
+    contrast_vect <- verify_factor(object, contrast_vect, factor)
+    
+    #Makes result table for each count table in list
+    for (i in 1:length(tbl_lst)){
+        #Makes object
+        cluster <- DESeqDataSetFromMatrix(tbl_lst[[i]],  #Data Table
+                                          colData = meta_tbl,  #metaData
+                                          design = form_fact)
+
+        #Filters out insignificant parts
+        cluster <- cluster[ rowSums(counts(cluster)) > 5, ]
+        cluster <- DESeq(cluster)
+
+        #Makes results obj
+        results <- results(cluster,
+                           contrast = contrast_vect,
+                           alpha = 0.05,
+                           pAdjustMethod = "fdr")
+
+        #Turns results into a Tibble
+        results_tib <- results %>%
+                       data.frame() %>%
+                       rownames_to_column(var="gene") %>%
+                       as_tibble()
+
+        #returns results
+        agg_res <- list.append(agg_res, clust_nam = results_tib)
+        names(agg_res)[i] <- names(tbl_lst)[i]
+    }
+
+    return(agg_res)
 }
