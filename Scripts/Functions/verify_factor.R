b27f822de0ddb158083ba64fb7af93e1fd99b3a7
DJT333 <135276784+DJT333@users.noreply.github.com>
2023-07-11 12:01:05 -0400

    Update verify_factor.R
    Changed this from my old verify to the new one. 
    Big Changes:
    -The tabs are 4 instead of 2
    -This can detect and yell at user for dupes
    -mod is in a better place
    -no longer case sensitive

--- Scripts/Functions/verify_factor.R
+++ Scripts/Functions/verify_factor.R
@@ -11,39 +11,71 @@
 #' @return the user's (corrected) choice vector
 #' @export
 verify_factor <- function(object, vect, factor){
-  
-  #makes vector of valid options
-  valids <- unique(object[[factor]])[,1]
-  
-  #Loops vector
-  for (i in 1:length(vect)){
-    #There is a case where factor is the first elem so this ensure output to user
+    #makes vector of valid options, normal and lowercase
+    valids <- unique(object[[factor]])[, 1]
+    valids_low <- tolower(valids)
+    
+    #There is a case where factor is the first elem so this ensures output to user
     #is consistent
     mod <- 0 
-    if (i == 1){
-      if (vect[i] != factor){
-        #Loops until valid input
-        while (!(vect[i] %in% valids)){
-          cat("Invalid input for Comparison", i, ". Please check for typo.\n",
-              "Valid inputs include:\n", sep = "")
-          sapply(valids, print, quote = FALSE)
-          vect[i] = readline(paste("Comparsion", i, ": ", sep = ""))
+    
+    #Loops vector
+    for (i in 1:length(vect)){
+        #Checks if first elem is the factor
+        if (i == 1){
+            if (vect[i] != factor){
+                #Loops until valid input
+                while (!(vect[i] %in% valids)){
+                    #checks for case, converts to proper if not
+                    if (tolower(vect[i]) %in% valids_low){
+                        vect[i] <- valids[match(tolower(vect[i]), valids_low)]
+                    }else {
+                        #prompts user about 
+                        cat("Invalid input for Comparison", i, 
+                            ". Please check for typo.\n",
+                            "Valid inputs include:\n", sep = "")
+                        sapply(valids, print, quote = FALSE)
+                        vect[i] = readline(paste("Comparsion", i, 
+                                                 ": ", sep = ""))
+                    }
+              }
+              #If the factor is the first index, i needs to be 1
+            } else {
+                mod <- 1
+            }
+        }else {
+            #Loops until valid input
+            while (!(vect[i] %in% valids)){
+                #checks for case 
+                if (tolower(vect[i]) %in% valids_low){
+                    vect[i] <- valids[match(tolower(vect[i]), valids_low)]
+                }else {
+                    cat("Invalid input for Comparison", i, 
+                        ". Please check for typo.\n",
+                        "Valid inputs include:\n", sep = "")
+                    sapply(valids, print, quote = FALSE)
+                    vect[i] = readline(paste("Comparsion", i, ": ", sep = ""))
+              }
+            }
         }
-        #If the factor is the first index, i needs to be 1
-      } else {
-        mod <- 1
-      }
-    }else {
-      #Loops until valid input
-      while (!(vect[i] %in% valids)){
-        cat("Invalid input for comparison", (i + mod), ". Please check for typo.\n",
-            "Valid inputs include:\n", sep = "")
-        sapply(valids, print, quote = FALSE)
-        vect[i] = readline(paste("comparison", (i + mod), ": ", sep = ""))
-      }
     }
-  }
-  
-  #returns vect with changes (if any)
-  return(vect)
+    
+    #dupe check
+    if (length(vect) > 1){
+        for (i in (1 + mod):(length(vect) - 1)){
+            for (j in (2 + mod):length(vect)){
+                while (vect[i] == vect[j]){
+                    cat("Dupe found at Comparsion", j - mod,
+                        ". Please use a different option within your set.\n",
+                        "Valid inputs include:\n", sep = "")
+                    sapply(valids, print, quote = FALSE)
+                    cat("Please don't put in the same thing in...")
+                    vect[j] = readline(paste("Comparsion", j - mod, ": ", sep = ""))
+                }
+            }
+        }
+    }
+    
+    #returns vect with changes (if any)
+    return(vect)
 }
