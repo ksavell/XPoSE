e0544cd08aafab12ce26776f6c4fc16ccd7a3228
DJT333 <135276784+DJT333@users.noreply.github.com>
2023-07-20 16:43:24 -0400

    Update prep_merge.R
    minor changes

--- Scripts/Functions/prep_merge.R
+++ Scripts/Functions/prep_merge.R
@@ -15,26 +15,29 @@
 prep_merge <- function(res_lst, object){
 
     source("~/Documents/GitHub/XPoSE/Scripts/Functions/bi_search.R")
-    
+    #Tracks time function took to run
     timer <- proc.time()
+    
     #will be returned at function's end
     return_lst <- list()
     
     ind <- 0
-    
+
+    #goes through entire results list
     for (cluster in names(res_lst)) {
+        #Tells user where function is in correction process
         ind <- ind + 1
         print(paste("On Index ", ind, "/", length(res_lst), sep = ""), quote = FALSE)
         
         #allows us to take rows from the base tibble
         cls_frm <- data.frame(res_lst[[cluster]])
         
-        #makes what will be new tibble and fills genes
+        #new tibble that will added to list
         new_tbl <- data.frame(matrix(
                       nrow = length(object@assays[["RNA"]]@counts@Dimnames[[1]]),
                       ncol = ncol(res_lst[[cluster]])))
         
-        #Adds some necessary data to new tibble
+        #Adds necessary data to new tibble
         new_tbl[, 1] <- object@assays[["RNA"]]@counts@Dimnames[[1]]
         colnames(new_tbl) <- colnames(cls_frm)
         rownames(cls_frm) <- cls_frm[, "gene"]
@@ -42,22 +45,23 @@ prep_merge <- function(res_lst, object){
         #fills shared rows w/ base data
         for (i in 1:nrow(new_tbl)) {
             if(!is.na(bi_search(cls_frm[, 1], new_tbl[i, 1]))){
-                #replaces row with data in cluster
+                #replaces row
                 new_tbl[i, ] <- cls_frm[bi_search(cls_frm[, 1], new_tbl[i, 1]), ]
             }
         }
         
         #returns to tibble and apppends to list
         new_tbl <- tibble(new_tbl)
-        
         return_lst <- list.append(return_lst, new_tbl)
     }
     
     #ensures names are same
     names(return_lst) <- names(res_lst)
     
-    el <- (proc.time() - timer)[[3]]
-    print(paste("Process took", round(el, digits = 3), "seconds"), quote = FALSE)
+    #tells user how long function took
+    elpd <- (proc.time() - timer)[[3]]
+    print(paste("Process took", round(elpd, digits = 3), "seconds"), 
+          quote = FALSE)
     
     #returns new list
     return(return_lst)
