68f12cf61e6f8d63123970f1202676f5a50cf991
Padmashri Saravanan <psarava1@jhu.edu>
2023-08-02 11:45:06 -0400

    mean table works now!

--- Scripts/figure1.R
+++ Scripts/figure1.R
@@ -23,17 +23,17 @@ make_stdf <- function(seur_obj) {
         IDs$cart <- seur_obj@meta.data[["orig.ident"]]
         IDs$rat <- seur_obj@meta.data[["ratID"]]
         IDs$st <- seur_obj@meta.data[["Sample_tag"]]
-        IDs$st2 <- seur_obj@meta.data[["ST2_reads"]]
-        IDs$st3 <- seur_obj@meta.data[["ST3_reads"]]
-        IDs$st4 <- seur_obj@meta.data[["ST4_reads"]]
-        IDs$st5 <- seur_obj@meta.data[["ST5_reads"]]
-        IDs$st6 <- seur_obj@meta.data[["ST6_reads"]]
-        IDs$st7 <- seur_obj@meta.data[["ST7_reads"]]
-        IDs$st8 <- seur_obj@meta.data[["ST8_reads"]]
-        IDs$st9 <- seur_obj@meta.data[["ST9_reads"]]
+        IDs$st2 <- seur_obj@meta.data[["SampleTag02_reads"]]
+        IDs$st3 <- seur_obj@meta.data[["SampleTag03_reads"]]
+        IDs$st4 <- seur_obj@meta.data[["SampleTag04_reads"]]
+        IDs$st5 <- seur_obj@meta.data[["SampleTag05_reads"]]
+        IDs$st6 <- seur_obj@meta.data[["SampleTag06_reads"]]
+        IDs$st7 <- seur_obj@meta.data[["SampleTag07_reads"]]
+        IDs$st8 <- seur_obj@meta.data[["SampleTag08_reads"]]
+        IDs$st9 <- seur_obj@meta.data[["SampleTag09_reads"]]
         
         rownames(IDs) <- IDs$seur_obj.assays...RNA....data.Dimnames..2..
-        IDs <- IDs %>% select(cart, rat, st)
+        #IDs <- IDs %>% select(cart, rat, st)
         
         return(IDs)
 }
@@ -50,10 +50,10 @@ mean <- sapply(IDslist, function(x) {
         # Ignore character columns
         numeric_cols <- numeric_cols & !sapply(x, is.character)
         
-        # Find the medians of the numeric columns
+        # Find the means of the numeric columns
         means <- apply(x[, numeric_cols], 2, mean)
         
-        # Return a named vector of medians
+        # Return a named vector of means
         names(means) <- names(x)[numeric_cols]
         return(means)
 })
