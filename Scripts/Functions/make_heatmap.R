4527741cbf7744e5a081adf5b723a67ff81bf15c
Padmashri Saravanan <psarava1@jhu.edu>
2023-08-02 14:30:04 -0400

    automates the function now!

--- Scripts/Functions/make_heatmap.R
+++ Scripts/Functions/make_heatmap.R
@@ -1,29 +1,39 @@
-#From Kareem
-
-glut.markers <- FindAllMarkers(glut.subset2, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
-glut.markers %>% 
+#' Creates Heatmap and writes the CSV file of the top markers
+#'
+#' @param data Seurat object of interest
+#' @param csvfilen file name for the CSV
+#' @param pdffilen file name for the Heatmap PDF
+#' @param groupcol a vector of colors to use for the heatmap according to cluster
+#'
+#' @return csv file + pdf of heatmap
+#' @export
+make_heatmap <- function(data, csvfilen = "topgenemarkers", pdffilen = "heatmap", groupcol){
+    
+    data.markers <- FindAllMarkers(data, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+    data.markers %>% 
         group_by(cluster) %>% 
-        top_n(n = 10, wt = avg_log2FC) -> top10.glut
-
-top10.glut.df <- as.data.frame(top10.glut)
-
-write.csv(top10.glut.df, file = "/Users/woodskad/Library/CloudStorage/Box-Box/mRFP-snSeq/Project1_XPoSEseq/XPoSEseq_manuscript/S3_panels/top10.glut.csv")
-
-glut.genes <- data.frame(table(top10.glut$gene))
-colnames(glut.genes)[1] = "Gene"
-df2 <- data.frame(Gene = "Slc17a7", Freq = 1)
-glut.genes2 = rbind(glut.genes, df2)
-
-pdf(file = "/Users/woodskad/Library/CloudStorage/Box-Box/mRFP-snSeq/Project1_XPoSEseq/XPoSEseq_manuscript/S3_panels/Heatmaps/glut_top10genes_heatmap052423.pdf",
-    width = 3.5,
-    height = 4.5)
-glut.heatmap <- DoHeatmap(object = glut.subset2, 
-                          features = top10.glut$gene,
-                          group.colors =  c("#64C7C8", "#41B75F", "#2C8CB9", "#0A5B8C", "#3C9E64", "#6F499D"),
-                          label = (FALSE),
-                          raster = TRUE)+
-        
-        theme(axis.title = element_blank(), axis.text.y = element_text(face = "italic", size = 5), legend.position = "none")+
-        
-        glut.heatmap
-dev.off()
+        top_n(n = 10, wt = avg_log2FC) -> top10.data
+    
+    top10.data.df <- as.data.frame(top10.data)
+    
+    write.csv(top10.data.df, 
+              file = paste0(csvfilen,".csv"))
+    
+    data.genes <- data.frame(table(top10.data$gene))
+    colnames(data.genes)[1] = "Gene"
+    
+    pdf(file = paste0(pdffilen,".pdf"),
+        width = 3.5,
+        height = 4.5)
+    data.heatmap <- DoHeatmap(object = data, 
+                              features = top10.data$gene,
+                              label = (FALSE),
+                              group.colors = groupcol,
+                              raster = TRUE) +
+        theme(axis.title = element_blank(),
+              axis.text.y = element_text(face = "italic", size = 5),
+              legend.position = "none")
+    
+    print(data.heatmap)
+    dev.off()
+}
