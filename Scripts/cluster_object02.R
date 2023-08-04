75322431001ed323c97297f640bb6425fb12b0ec
Katherine Savell <katherinesavell@gmail.com>
2023-08-04 10:52:18 -0400

    done done

--- Scripts/cluster_object02.R
+++ Scripts/cluster_object02.R
@@ -6,7 +6,6 @@
 #       * glut subset object
 #       * gaba subset object
 
-
 # Loading -----------------------------------------------------------------
 ## Load packages -----------------------------------------------------------
 # loads all required packages
@@ -38,35 +37,46 @@ clstr_vln(combined, all = T)
 # 26, 28, 30 are minor glia contamination
 # 24 is low quality
 
-# Subset glut and gaba ----------------------------------------------------
+# Subset glut -------------------------------------------------------------
 
 #Specify which clusters to keep
 keep_glut <- as.character(c(0:8,12:18,20,22,25))
 
 source("~/XPoSE/Scripts/Functions/subset_reclust.R")
 
-glut <- subset_reclust(combined, clust_tokeep = keep_glut, neigh_dim = 1:30, umap_dim = 1:30, res = 0.2)
+glut <- subset_reclust(combined, clust_tokeep = keep_glut, neigh_dim = 1:30, 
+                       umap_dim = 1:30, res = 0.2)
 
-# name the final clusters
+# check glut clusters for known markers
+clstr_vln(glut, glut = T)
 
+# name the final clusters and save
 glut_ids <- c("IT_L5/6", "IT_L2/3", "CT_L6", "PT_L5", "NP_L5/6", "CT_L6b")
 names(glut_ids) <- levels(glut)
 glut <- RenameIdents(glut, glut_ids)
 glut$cluster_name <- paste(glut@active.ident)
 save(glut, file = "glut.RData")
 
-## gaba time
+
+# Subset gaba -------------------------------------------------------------
+
 
 keep_gaba <- as.character(c(9:11,19,21,23,27,32))
 
-gaba <- subset_reclust(combined, clust_tokeep = keep_gaba, neigh_dim = 1:30, umap_dim = 1:30, res = 0.2)
+gaba <- subset_reclust(combined, clust_tokeep = keep_gaba, neigh_dim = 1:30, 
+                       umap_dim = 1:30, res = 0.2)
 
 # one more refinement
 
 keep_gaba2 <- as.character(c(0:2,4:7))
 
-gaba <- subset_reclust(gaba, clust_tokeep = keep_gaba2, neigh_dim = 1:25, umap_dim = 1:25, res = 0.2)
+gaba <- subset_reclust(gaba, clust_tokeep = keep_gaba2, neigh_dim = 1:25, 
+                       umap_dim = 1:25, res = 0.2)
 
+# check gaba clusters for known markers
+clstr_vln(gaba, gaba = T)
+
+# name final clusters and save
 gaba_ids <- c("Pvalb", "Sst", "Ppp1r1b", "Vip", "Meis2", "Lamp5", "Sst Chodl")
 names(gaba_ids) <- levels(gaba)
 gaba <- RenameIdents(gaba, gaba_ids)
