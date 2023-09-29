98ab3e11ab2129ad182eec8322ef57e44dbb5ab9
Katherine Savell <katherinesavell@gmail.com>
2023-09-29 15:48:32 -0400

    order fix

--- Scripts/figure2.R
+++ Scripts/figure2.R
@@ -2,10 +2,11 @@
 
 # This script generates:
 # Figure 2a, excitatory/inhibitory DimPlots
-# Figure 2b, nuclei counts by group table*
-# Figure 2c, excitatory/inhibitory DimPlots grouped by group
+# Figure 2b, excitatory/inhibitory DimPlots grouped by group
+# Figure 2c, nuclei prop by group table*
 # Figure 2d, DEG table*
-# Figure 2e, DEG upset table*
+# Figure 2e, log2FC for example genes*
+# Figure 2f, DEG upset table*
 
 # * denotes that the final plot was made in Prism with output generated in R
 
@@ -27,7 +28,6 @@ library(data.table)
 
 # Load data ---------------------------------------------------------------
 
-setwd("~/XPoSE")
 load("glut.RData")
 load("gaba.RData")
 setwd("~/XPoSE/Output")
@@ -40,15 +40,13 @@ save_dimplot(glut, file_n = "glutclusters.pdf", glutcol = T)
 
 save_dimplot(gaba, file_n = "gabaclusters.pdf", gabacol = T)
 
-# F2b,d DimPlots and proportions by group ---------------------------------
-
-# left, group distribution 
+# F2b DimPlots and proportions by group ---------------------------------
 
 save_dimplot(glut, groupby = "group", file_n = "glutgroup.pdf", groupcol = T)
 
 save_dimplot(gaba, groupby = "group", file_n = "gabagroup.pdf", groupcol = T)
 
-# right, proportion calculation
+# F2c proportions by group ----------------------------------------------
 source("~/XPoSE/Scripts/Functions/calc_prop.R")
 
 glut_group <- calc_prop(seur_obj = glut, fact1 = 'ratID',
@@ -61,7 +59,7 @@ gaba_group <- calc_prop(seur_obj = gaba, fact1 = 'ratID',
                           fact3 = 'group',
                           file_n = "gaba_group.csv")
 
-# F2e DEG table -----------------------------------------------------------
+# F2d DEG table -----------------------------------------------------------
 
 source("~/XPoSE/Scripts/Functions/make_coexp.R")
 
@@ -86,15 +84,7 @@ coexp_AN <- make_coexp(seur_obj1 = glut, seur_obj2 = gaba,
 
 write.csv(coexp_AN, file = "~/Output/coexp_AN.csv")
 
-# F2f Upset table ---------------------------------------------------------
-
-source("~/XPoSE/Scripts/Functions/prep_upset.R")
-source("~/XPoSE/Scripts/Functions/make_upset.R")
-
-make_upset(prep_upset(coexp_AN), 
-           order_vect = c("PTL5","ITL23","ITL56","CTL6","Pvalb","Sst"))
-
-# F2g examples ------------------------------------------------------------
+# F2e examples ------------------------------------------------------------
 
 source("~/XPoSE/Scripts/Functions/find_DEcounts.R")
 
@@ -108,3 +98,10 @@ find_DEcounts(PTL5_dds, coexp_AN, "PTL5", highlights)
 find_DEcounts(Pvalb_dds, coexp_AN, "Pvalb", highlights)
 find_DEcounts(Sst_dds, coexp_AN, "Sst", highlights)
 
+# F2f Upset table ---------------------------------------------------------
+
+source("~/XPoSE/Scripts/Functions/prep_upset.R")
+source("~/XPoSE/Scripts/Functions/make_upset.R")
+
+make_upset(prep_upset(coexp_AN), 
+           order_vect = c("PTL5","ITL23","ITL56","CTL6","Pvalb","Sst"))
