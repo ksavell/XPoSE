e3f0341a126730dd6ea60c97631b7a98e8044499
Katherine Savell <61556167+ksavell@users.noreply.github.com>
2024-07-24 12:11:52 -0400

    tally_plot: cleaned up and made for loop to iterate through clusters

--- Scripts/tally_plot.R
+++ Scripts/tally_plot.R
@@ -8,7 +8,7 @@ source("Scripts/Functions/tally_iterations.R")
 
 # load results first 
 
-clstr <- "ITL23"
+clstr <- "ITL23" # change clusters here
 
 files <- list.files(clstr, pattern = "^results_.*\\.RData$", full.names = TRUE)
 data_list <- lapply(files, function(file) {
@@ -22,53 +22,9 @@ names(data_list) <- basename(files)
 # remove the file extension from the names
 names(data_list) <- sub("\\.RData$", "", names(data_list))
 
-# now tally the results
+# now tally the results and save that csv 
 for (j in names((data_list))) {
   merged_results_df <- tally_iterations(data_list, j)
-  write.csv(merged_results_df, file = paste0(clstr,"/",j,"_iteration_tally.csv"))
+  write.csv(merged_results_df, 
+            file = paste0(clstr,"/",j,"_iteration_tally.csv"))
 }
-
-
-
-
-
-
-
-
-
-
-
-# count occurrence of +1 in each row for each tally and write into a new df
-
-count_observed <- data.frame(p035 = (rowSums(ITL23p_035iteration_tally == 1, na.rm = TRUE)),
-                             p10 = (rowSums(ITL23p_10iteration_tally == 1, na.rm = TRUE)),
-                             p25 = (rowSums(ITL23p_25iteration_tally == 1, na.rm = TRUE)),
-                             p50 = (rowSums(ITL23p_50iteration_tally == 1, na.rm = TRUE)),
-                             p75 = (rowSums(ITL23p_75iteration_tally == 1, na.rm = TRUE)),
-                             p100 = (ITL23_100iteration_tally$score_column),
-                             row.names = rownames(ITL23p_035iteration_tally))
-
-# subset rows that have at least one +1 in the 100 iteration tally
-row_keep <- which(count_observed$p100 == 1)
-
-count_observed <- count_observed[row_keep,]
-
-# Function to calculate mean and SEM
-calc_mean_sem_count <- function(column) {
-  
-  # Calculate mean
-  mean_value <- mean(column)
-  
-  # Calculate SEM
-  sem_value <- sd(column) / sqrt(length(column))
-  
-  return(c(mean = mean_value, SEM = sem_value))
-}
-
-# Apply the function to each column in the data frame
-result <- lapply(count_observed, calc_mean_sem_count)
-
-# Convert the result to a data frame
-result_df <- as.data.frame(do.call(rbind, result))
-
-write_csv(result_df, "ITL23_prop_observed.csv")
