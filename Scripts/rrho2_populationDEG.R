6a09aff6b23b41fa930a92280260c0f585864447
Katherine Savell <katherinesavell@gmail.com>
2025-01-30 16:53:19 -0500

    plotting in ComplexHeatmap
    to be determined: how many NA's is it inserting to break up the 4 quadrants?

--- Scripts/rrho2_populationDEG.R
+++ Scripts/rrho2_populationDEG.R
@@ -1,12 +1,12 @@
 
-library(devtools)
+
 library(RRHO2)
 
 #Load in csv files of population deg comparisons per cluster
 
-ITL23_A_HC <- read.csv("/Users/holmesar/Library/CloudStorage/Box-Box/RM_Projects/mRFP-snSeq/Project1_XPoSEseq/XPoSEseq_manuscript/NeuroResource_January2025/DataForFigures/F5/population_degs/ITL23_group_Active_Homecage.csv")
-ITL23_A_NA <- read.csv("/Users/holmesar/Library/CloudStorage/Box-Box/RM_Projects/mRFP-snSeq/Project1_XPoSEseq/XPoSEseq_manuscript/NeuroResource_January2025/DataForFigures/F5/population_degs/ITL23_group_Active_Nonactive.csv")
-ITL23_NA_HC <- read.csv("/Users/holmesar/Library/CloudStorage/Box-Box/RM_Projects/mRFP-snSeq/Project1_XPoSEseq/XPoSEseq_manuscript/NeuroResource_January2025/DataForFigures/F5/population_degs/ITL23_group_Nonactive_Homecage.csv")
+ITL23_A_HC <- read.csv("~/Library/CloudStorage/Box-Box/RM_Projects/mRFP-snSeq/Project1_XPoSEseq/XPoSEseq_manuscript/NeuroResource_January2025/DataForFigures/F5/population_degs/ITL23_group_Active_Homecage.csv")
+ITL23_A_NA <- read.csv("~/Library/CloudStorage/Box-Box/RM_Projects/mRFP-snSeq/Project1_XPoSEseq/XPoSEseq_manuscript/NeuroResource_January2025/DataForFigures/F5/population_degs/ITL23_group_Active_Nonactive.csv")
+ITL23_NA_HC <- read.csv("~/Library/CloudStorage/Box-Box/RM_Projects/mRFP-snSeq/Project1_XPoSEseq/XPoSEseq_manuscript/NeuroResource_January2025/DataForFigures/F5/population_degs/ITL23_group_Nonactive_Homecage.csv")
 
 
 A_HC <- ITL23_A_HC
@@ -58,7 +58,7 @@ gene_list1 <- merged_df[, c("gene", "DDE_A_NA")]
 gene_list2 <- merged_df[, c("gene", "DDE_NA_HC")]
 
 # Now initialize the RRHO2 object
-RRHO_obj <- RRHO2_initialize(gene_list1, gene_list2, labels = c("A_NA", "NA_HC"), log10.ind = TRUE, stepsize = 10)
+RRHO_obj <- RRHO2_initialize(gene_list1, gene_list2, labels = c("A_NA", "NA_HC"), log10.ind = TRUE, stepsize = 100)
 
 # RRHO_obj <- RRHO2_initialize(
 #   gene_list1,
@@ -70,6 +70,22 @@ RRHO_obj <- RRHO2_initialize(gene_list1, gene_list2, labels = c("A_NA", "NA_HC")
 #   boundary = 0.1,
 #   method = "hyper"
 # )
+
+
+
+rrho_matrix <- as.matrix(RRHO_obj[["hypermat"]])
+
+
+custom_colors <- colorRamp2(
+  c(0, 1.3, 1.305, 10, 50),  # Corresponding -log10(pval) values (e.g., 0.05, 0.01, etc.)
+  c("gray90", "gray90", "paleturquoise", "paleturquoise3","paleturquoise4")  # Colors mapped to these values
+)
+
+rrho_matrix[is.na(rrho_matrix)] <- 0
+
+Heatmap(rrho_matrix, col = custom_colors, name = "-log10(pval)", cluster_rows = FALSE, cluster_columns = FALSE)
+
+
 pdf("bin10_ANA_NAHC_rrho.pdf", height = 20, width = 20)
 RRHO2_heatmap(RRHO_obj, colorGradient = )
 dev.off()
